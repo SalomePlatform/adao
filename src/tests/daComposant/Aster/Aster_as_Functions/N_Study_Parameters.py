057a095dd5118c850caa4ce1455b11648197a646
André Ribes <andre.ribes@edf.fr>
2011-01-26 15:42:55 +0100

    Suppression des tests de la bibliothéque

--- src/tests/daComposant/Aster/Aster_as_Functions/N_Study_Parameters.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/python
-
-"""
-   Code_Aster_Study Parameters
-"""
-
-#export='/home/assire/DEV/MACR_RECAL/YACS/zzzz159a/zzzz159a.export'
-#export='/home/ahbhhjp/SALOME5/Aster/YACS/zzzz159a/zzzz159a.export.esclave'
-export='/home/ahbhhjp/SALOME5/supplements_JPA/Aster/Aster_as_Functions/zzzz159a/zzzz159a.export.esclave'
