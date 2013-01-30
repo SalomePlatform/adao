c73864264aff5fcc69c24cdcbc84af0d3e0179a6
Jean-Philippe ARGAUD <jean-philippe.argaud@edf.fr>
2013-01-30 15:28:31 +0100

    Correcting date copyright and version name information

--- src/daComposant/daCore/version.py
+++ src/daComposant/daCore/version.py
@@ -1,6 +1,6 @@
 #-*-coding:iso-8859-1-*-
 #
-# Copyright (C) 2008-2012 EDF R&D
+# Copyright (C) 2008-2013 EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -21,5 +21,5 @@
 # Author: Jean-Philippe Argaud, jean-philippe.argaud@edf.fr, EDF R&D
 
 name    = "Data Assimilation Package"
-version = "0.6.0"
-date    = "jeudi 08 juin 2012, 11:11:11 (UTC+0200)"
+version = "0.7.0"
+date    = "mercredi 12 décembre 2012, 12:12:12 (UTC+0100)"
