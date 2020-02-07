d98638db700717abd2ecbacb8472940ec3890d07
Jean-Philippe ARGAUD <jean-philippe.argaud@edf.fr>
2020-02-07 11:15:57 +0100

    Updating ADAO user documentation copyright (Tuleap [bos #18697])

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -78,7 +78,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'%s'%module_version.name
-copyright = u'2008-%s, Jean-Philippe ARGAUD'%module_version.year
+copyright = u'2008-%s, EDF R&D, Jean-Philippe ARGAUD'%module_version.year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
